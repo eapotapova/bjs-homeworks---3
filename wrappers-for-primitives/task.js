6bf548402a9d26934a4d708cac2af91d8e4d4cbe
eapotapova <epotapova1104@gmail.com>
2019-04-08 23:55:20 +0300

    Wrappers-for-primitives: first task

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+"use strict"
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,9 +12,45 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+ 
+ let newDate = new Date(date);
+ let today = new Date();
+ let valid;
+ 
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+ if (isNaN(Number(percent)) === true || Number(percent) < 0 ) {
+  console.log( `Параметр <percent> содержит неправильное значение ${percent}`)
+   valid = false;
+ } else if (isNaN(Number(contribution)) === true || Number(contribution) < 0) {
+    console.log( `Параметр <contribution> содержит неправильное значение ${contribution}`)
+    valid = false;
+} else if (isNaN(Number(amount)) === true || Number(amount) <= 0) {
+    console.log( `Параметр <amount> содержит неправильное значение ${amount}`)
+    valid = false;
+} else if (newDate.getFullYear() <= today.getFullYear() ) {
+    console.log( `Параметр <date> содержит неправильное значение ${date}`)
+    valid =false; 
+}
+
+ if (valid===false) {
+     return
+ }
+ 
+ 
+ let creditSum = Number(amount) - Number(contribution);
+ let month = (newDate.getFullYear() -  today.getFullYear()) * 12 + (newDate.getMonth() - today.getMonth())
+
+ let payment = creditSum*((Number(percent)/100)/12 + ((Number(percent)/100)/12)/(((1+(Number(percent)/100)/12)**month)-1))
+
+ let totalAmount = payment*month
+ 
+ 
+
+
+
+
+    
+ return totalAmount;
 }
 
 function sayHello() {
@@ -23,6 +61,10 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    
+    
+    console.log(typeof name);
+    let greeting = `Привет, мир! Меня зовут ${name}`
+
+    return greeting;
 }
